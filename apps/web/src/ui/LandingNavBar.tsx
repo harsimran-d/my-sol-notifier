601eb967fbbc75321b112ebb483656c2515762e3
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-10 13:41:23 +0530

    create landing page and navbar

---
+++ apps/web/src/ui/LandingNavBar.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+export default function LandingNavBar() {
+  const session = useSession();
+  const router = useRouter();
+  return (
+    <div className="flex justify-end bg-blue-500 p-4 text-xl text-white">
+      {session.status == "authenticated" ? (
+        <button onClick={() => router.push("/dashboard")}>Dashboard</button>
+      ) : (
+        <button
+          onClick={() => router.push("/api/auth/signin?callbackUrl=/dashboard")}
+        >
+          Get Started
+        </button>
+      )}
+    </div>
+  );
+}
---
+++ apps/web/src/ui/LandingPage.tsx
@@ -0,0 +1,20 @@
+export default function LandingPage() {
+  return (
+    <div className="div mx-10 mt-3 justify-center space-y-6 divide-y-4 divide-blue-500 pt-3 text-center align-middle text-xl">
+      <p>
+        Sol-Notifier is a user-friendly notification platform designed for
+        cryptocurrency enthusiasts and traders, allowing them to seamlessly
+        connect their wallets to various notification sources.
+      </p>
+      <p className="pt-4">
+        It provides a straightforward way to stay updated on important events
+        across multiple blockchains without the hassle of checking each one
+        individually.
+      </p>
+      <p className="pt-4">
+        More than just a notification tool, Sol-Notifier enhances your crypto
+        experience by ensuring you never miss critical updates on your assets.
+      </p>
+    </div>
+  );
+}
