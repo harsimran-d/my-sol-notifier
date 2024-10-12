1aa1e062149d55f7e268e583f0eb4e2a60e9d262
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-12 23:29:25 +0530

    add the needed wallet adapter providers

--- apps/web/src/lib/providers.tsx
+++ apps/web/src/lib/providers.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </SessionProvider>
+  );
 }
