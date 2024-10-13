375371b4c8203e6259b1e29723db6f5bba11f321
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-13 15:17:21 +0530

    add the new components to dashboard page

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import SolWalletButtons from "@/ui/components/SolWalletButtons";
+import AddPubKeyToSolNotifier from "@/ui/components/AddPubKeyToSolNotifier";
+import MyAddedKeys from "@/ui/components/MyAddedKeys";
 
 export default function Dashboard() {
   return (
     <div>
       <div className="flex justify-end pr-2 pt-2">
         <SolWalletButtons />
+      </div>
+      <div className="m-auto flex w-8/12 flex-col justify-center">
+        <AddPubKeyToSolNotifier />
+        <MyAddedKeys />
       </div>
     </div>
   );
