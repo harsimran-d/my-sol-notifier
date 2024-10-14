89a98518cc0b5e9cb9f0a3ae67cb6f74aff7cb3c
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-14 16:16:01 +0530

    write code to show user the tokens that wallet owns

--- apps/web/src/app/dashboard/solana/tokens/[walletAddress]/page.tsx
+++ apps/web/src/app/dashboard/solana/tokens/[walletAddress]/page.tsx
@@ -1,12 +1,33 @@
+import { getTokens } from "@/actions/web3/getTokens";
+
 export default async function Page({
   params,
 }: {
   params: { walletAddress: string };
 }) {
+  const result = await getTokens(params.walletAddress);
   return (
     <div className="m-auto w-8/12">
       <div>In your wallet: {params.walletAddress} you have:</div>
       <p>Following Tokens</p>
+      <table className="min-w-full table-auto border-separate border-spacing-y-3 rounded-lg border-t-2 border-gray-400 bg-gray-100 p-4 shadow-lg">
+        <thead>
+          <tr>
+            <th className="text-left">Address</th>
+            <th className="text-left">Balance</th>
+            <th className="text-left">Decimals</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((tokenBalance: any) => (
+            <tr key={tokenBalance.pubKey}>
+              <td>{tokenBalance.pubkey}</td>
+              <td>{tokenBalance.balance}</td>
+              <td>{tokenBalance.decimals}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
